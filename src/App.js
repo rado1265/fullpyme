87cd32bfe3b16e74cbefb1781d222a70077b9e15
rado1265 <roberto.diaz.o@outlook.com>
2025-04-09 18:26:52 -0400

    funciona

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Layout from './layouts/Layout';
 
-function App() {
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datetime/css/react-datetime.css';
+import 'react-image-lightbox/style.css';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Layout />
+    </Router>
   );
-}
+};
 
 export default App;
